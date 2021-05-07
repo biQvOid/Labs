89bbf5666ef68dcb8f8b028ad8ef97d555d160f8
biQvOid <70909971+biQvOid@users.noreply.github.com>
2021-05-07 21:11:33 +0300

    Create matrix.h

---
+++ KP7/matrix.h
@@ -0,0 +1,14 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#include "vector.h"
+
+vector* matrix_input(vector* v);
+void task_print(vector* v);
+void natural_print(vector* v);
+void function(vector* v);
+void function1(vector* v);
+int el_count(char name[20]);
+int lines_count(char name[20]);
+
+#endif
