76aa9931571ef6444d003838198fca7951d44a67
biQvOid <70909971+biQvOid@users.noreply.github.com>
2021-05-15 03:18:11 +0300

    Create lexer.h

---
+++ lab24/lexer.h
@@ -0,0 +1,29 @@
+#ifndef __LEXER_H__
+#define __LEXER_H__
+
+#include <stdbool.h>
+
+typedef enum {
+    FINAL,
+    INTEGER,
+    FLOATING,
+    OPERATOR,
+    VARIABLE,
+    BRACKET
+} TokenType;
+
+typedef struct {
+    TokenType type;
+    union {
+        int   value_int;
+        float value_float;
+        char  operator_name;
+        bool  is_left_bracket;
+        char  variable_name;
+    } data;
+} Token;
+
+void token_print(Token *t);
+void token_next(Token *t);
+
+#endif
