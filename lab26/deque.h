9e7a42d91e849ff648582a43de34cb24e2cb84aa
biQvOid <70909971+biQvOid@users.noreply.github.com>
2021-05-24 19:04:11 +0300

    Create deque.h

---
+++ lab26/deque.h
@@ -0,0 +1,31 @@
+#ifndef DEQUE
+#define DEQUE
+
+#include <stdio.h>
+
+typedef struct _deque deque;
+
+struct _deque
+{
+    int* data;
+    int capasity;
+    int number_of_elements;
+};
+
+void deque_create(deque* a);
+void push_front(deque* a, int b);
+void push_back(deque* a, int b);
+int first_front(deque* a);
+int first_back(deque* a);
+int empty(deque* a);
+void pop_front(deque* a);
+void pop_back(deque* a);
+int empty(deque* a);
+void resize(deque* a);
+int size(deque* a);
+
+deque* reverse(deque* a);
+void merge(deque* res, deque* a, deque* b);
+void merge_sort(deque* a);
+
+#endif
